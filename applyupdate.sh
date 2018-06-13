df043df30dfe8db5c44f8e16661cc86eb6f7e84d
I306445 <allen.yin@sap.com>
2018-06-13 17:57:54 +0800

    init

--- applyupdate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-new_version=$1
-
-echo ${new_version}
-
-sed -e "s/{{ base_url }}/${new_version}/g" \
-    < deployment_dev.yml \
-    > deployment.yml
--- kubestatup.sh
+++
@@ -1,6 +0,0 @@
-#/bin/bash
-
-KUBEMasterAddress=10.129.126.224:8080
-kubectl apply -f deployment.yml --record --server=$KUBEMasterAddress
-
-kubectl describe svc triggerbuildsv-microservice --server=$KUBEMasterAddress
